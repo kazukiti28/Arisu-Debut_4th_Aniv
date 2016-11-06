1389da89045e879c7fdb6fecbcb3f49b5e2e4c65
Kazuki F <Anzu@Anzu.local>
2016-11-06 19:54:04 +0900

    Initialize

---
+++ js/click.js
@@ -0,0 +1,30 @@
+function imgClick() {
+	$("#main").animate({
+		'top':'-1000px'
+	});
+	$(".first").fadeOut(500);
+
+	$(".crack").fadeIn(1000);
+	$('.crack').animate({
+		"left": "-=720px", "top": "270px"
+	},1400);
+	setTimeout(function(){
+		$(".flipcrack").fadeIn(1000);
+		$('.flipcrack').animate({
+			"left": "720px", "top": "270px"
+		},1200);
+	},1500);
+	$("#main").fadeOut(1);
+	setTimeout(function(){
+		$(".ichigoarisu").fadeIn(300);
+	},3000);
+	setTimeout(function(){
+		$(".ichigo").fadeIn(300);
+	},2000);
+	setTimeout(function(){
+		$(".mess").fadeIn(300);
+	},1000);
+		setTimeout(function(){
+		$(".message").fadeIn(600);
+	},4000);
+}
